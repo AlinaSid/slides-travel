d76772e724146452a4f916c5ec2025b87b47abba
AlinaSid <alinanice@list.ru>
2024-01-07 22:16:19 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
+import { data } from './data';
+import { useState } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const  [place,newPlace]= useState(0)
+const {id,placeName,image,description}=data[place]
+
+const removePlace=(id)=>{
+  let rePlace = place.filter(placeName=>placeName.id !==id);
+  newPlace(rePlace)
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+  const [showMore,setShowMore]=useState(false)
+
+
+
+
+const previousPlace =()=> {
+  newPlace((place=>{
+    place--;
+    if (place<0){
+      return data.length-1;
+    }
+    return place;
+  }))
+}
+
+
+const nextPlace =()=>{
+  newPlace((place=>{
+    place++;
+    if (place>data.length-1){
+      place=0
+    }
+    return place;
+  }))
+}
+
+
+  return ( <div >
+    <div className='container'>
+      <h1>#{id} {placeName}</h1>
+    </div>
+    
+    <div className='container'>
+      <img src={image} width='700px' alt="place"/>
+    </div>  
+
+    <div className='container'>
+<button className='btn' onClick={previousPlace}><img src='https://img.icons8.com/?size=100&id=39776&format=png'/> </button>
+
+<button className='btn' onClick={()=>removePlace(id)}>i'm not interesting/remove it</button>
+
+<button className='btn' onClick={nextPlace}><img src='https://img.icons8.com/?size=100&id=39777&format=png'/></button>
+    </div>
+      
+
+    <div className='container'>
+    <h2>{showMore ? description : description.substring(0,300)+ "..."}
+    <button className='btn2' onClick={()=>setShowMore(!showMore)}>{showMore ? "Show less" : "Show more"} </button>
+    </h2>
+    </div> 
+
+    <div className='container'>
+      <button className='btn3' onClick={()=>newPlace([])}>Delete All</button>
+    </div>
+
+  
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,53 @@
+export const data = [
+    {
+  id:1,
+  placeName:"Yellowstone Park",
+  image:"https://a.cdn-hotels.com/gdcs/production147/d1071/bebd0f9e-892b-4643-a23b-92af0ee3ae31.jpg?impolicy=fcrop&w=1600&h=1066&q=medium",
+  description:"Yellowstone National Park is world-renowned for its epic landscapes, unique wildlife, and fascinating natural history. Each year millions of people come from all over the globe to witness Yellowstone’s wonders. The stunning Grand Prismatic Spring is a famous thermal feature in Yellowstone National Park due to its coloration and size. The Grand Prismatic Spring is massive, with a diameter that is bigger than a football field and a depth of 121 feet. Its most unique feature is the mixture of orange, yellow, green, and blue waters that form a vibrant and multilayered effect. The hue and depth of the colors change with the seasons, depending on what bacteria are thriving at that particular time, so you will never see the same image twice. ",
+    },
+
+    {
+  id:2,
+  placeName:"Hawaiʻi Volcanoes National Park",
+  image:"http://gsavia.com/wp-content/uploads/2016/05/03.jpg",
+  description:"Hawai‘i Volcanoes National Park protects some of the most unique geological, biological, and cherished cultural landscapes in the world. Extending from sea level to 13,680 feet, the park encompasses the summits of two of the world's most active volcanoes - Kīlauea and Mauna Loa - and is a designated International Biosphere Reserve and UNESCO World Heritage Site.",
+    },
+
+    {
+        id:3,
+        placeName:"Angel Falls",
+        image:"http://www.vodopads.ru/Foto/Venezuela/Angel/vodopad_ankhel.jpg",
+        description:"Angel Falls, waterfall in the Guiana Highlands in Bolívar state, southeastern Venezuela, on the Churún River, a tributary of the Caroní, 160 miles (260 km) southeast of Ciudad Bolívar. The highest waterfall in the world, the cataract drops 3,212 feet (979 metres) and is 500 feet (150 metres) wide at the base. It leaps from a flat-topped plateau, Auyán-Tepuí (“Devils Mountain”), barely making contact with the sheer face. The falls are located in Canaima National Park, and, because of the dense jungle surrounding the falls, they are best seen from the air. The falls, first sighted by outsiders in the 1930s, were named for James Angel, an American adventurer who crash-landed his plane on a nearby mesa in 1937. In late 2009 Venezuelan Pres. Hugo Chávez declared that the falls should be referred to as Kerepakupai Merú, an Indigenous name."
+    },
+    {
+        id:4,
+        placeName:"Patagonia",
+        image:"https://www.rei.com/blog/wp-content/uploads/sites/4/2018/12/PAL_051316_39744-1.jpg?resize=1024%2C681",
+        description:"Patagonia is the southernmost region of South America, starting 1,000 kilometers or so (600 mi) south of Buenos Aires (the Argentine capital) and Santiago (the Chilean capital). Patagonia: a place of towering mountains that rise up to spear the sky; a mythical landscape scattered with cobalt blue glaciers and meandering fjords; a destination packed with braying penguins, breaching whales, and elusive pumas."
+    },
+
+    {
+        id:5,
+        placeName:"Antarctica",
+        image:"https://www.secretatlas.com/wp-content/uploads/2021/11/antarctica-photo-tour-1.jpg",
+        description:"Antarctica is a place of extremes. It is the coldest, highest, driest and windiest continent on Earth. Antarctica is a continent capped by an inland ice sheet up to 4.8km thick, containing approximately 90% of the world’s total surface fresh water (and 60% of the world’s total fresh water). Here it may be as cold as -75°C, but the world record lowest temperature is from an even more remote Antarctic station, Vostok, which logged -89°C. The extreme cold has kept the continent almost empty of life. Some mosses and liverworts grow in ice-free areas along the coast, and two types of flowering plants grow on the peninsula. Native land animals are limited to certain kinds of arthropods, or insectlike animals. About 45 kinds of birds live in Antarctica. he emperor and the Adélie penguins are found in large numbers around the entire coastline. Gentoo and chinstrap penguins occupy the Antarctic Peninsula coasts and some islands. Several kinds of seals and whales inhabit the waters around Antarctica. Fishes limited to the Antarctic include the Antarctic cod and the icefish."
+    },
+
+    {
+        id:6,
+        placeName:"Galapagos Islands",
+        image:"https://share.america.gov/wp-content/uploads/2023/05/GettyImages-521438982.jpg",
+        description:"Situated in the Pacific Ocean some 1,000 km from the South American continent, these 19 islands and the surrounding marine reserve . Ongoing seismic and volcanic activity reflects the processes that formed the islands. These processes, together with the extreme isolation of the islands, led to the development of unusual animal life – such as the land iguana, the giant tortoise and the many types of finch. As amazing as you think the Galápagos Islands will be, they routinely exceed expectations. It's a place where lizards swim, birds walk, and humans — for once — don't take center stage. This is also one of the world's top scuba diving destinations, so spending time on or in the water is a must, whether that means you're ready to jump in with your snorkel for a sea lion swim or are happy to enjoy the views from the deck of your adventure cruise. Brimming with natural beauty, the Galápagos is for many a once-in-a-lifetime destination where the marvels of the natural world are waiting to astound you.",
+    },
+
+    {
+        id:7,
+        placeName:"Mayan Pyramids",
+        image:"https://www.roadaffair.com/wp-content/uploads/2017/09/chichen-itza-mexico-shutterstock_346970900.jpg",
+        description:"There are over 4,400 Maya sites throughout Latin America, with many of them located in Mexico. One could easily spend a whole lifetime exploring all the ruins in Mexico. Features of Maya architecture include multi-level elevated platforms, massive step-pyramids, corbelled roofing, and monumental stairways.  Exteriors were decorated with sculpture and mouldings of Maya glyphs, geometric shapes, and iconography from religion such as serpent masks. nterestingly, unlike in many other cultures, Maya architecture makes no particular distinction between religious and non-religious buildings. Pyramids were used not only as temples and focal points for Maya religious practices where offerings were made to the gods but also as gigantic tombs for deceased rulers, their partners, sacrificial victims, and precious goods. Pyramids were also periodically enlarged so that their interiors, when excavated, sometimes reveal a series of complete but diminishing pyramids, often still with their original coloured stucco decoration. In addition, individual shrines could be amalgamated into a single giant complex over time as Maya rulers attempted to impress their subjects and leave a lasting mark of their reign. A good example of this development can be seen at the North Acropolis of Tikal."
+    }
+
+
+
+
+]
